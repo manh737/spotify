3b0f12f93a4c083005de0f5f89a1743e266d4d7b
manhnd <manhnd@fabbi.io>
2020-05-13 16:07:08 +0700

    edit api

--- include/header.php
+++ include/header.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?=SITENAME?> | <?=ucfirst(pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME))?></title>
-    <link rel="stylesheet" href="assets/css/vendor.css" />
-    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/vendor.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
   </head>
   <body>
 
